a015aad98468b8b25f13766e0299d4a529bbde4a
彭棕 <pengzong@laiye.com>
2023-03-29 17:37:54 +0800

    fix: 图床地址问题修复

--- internal/model/imghost.go
+++ internal/model/imghost.go
@@ -1,10 +1,10 @@
 package model
 
 import (
-	"goblog/config"
-	"goblog/internal/common"
 	"errors"
 	"github.com/kataras/iris"
+	"goblog/config"
+	"goblog/internal/common"
 	"io"
 	"os"
 	"path"
@@ -14,13 +14,12 @@ import (
 )
 
 type ImgHost interface {
-	ImageUpload(iris.Context) (string,error)
+	ImageUpload(iris.Context) (string, error)
 	ImageList() Images
 	ImageDel(string) bool
 }
 
 type ImageHost struct {
-
 }
 
 type Images []Image
@@ -31,22 +30,22 @@ type Image struct {
 	CreateTime time.Time `json:"create_time"`
 }
 
-func (list *ImageHost)ImageList() []Image {
+func (list *ImageHost) ImageList() []Image {
 	var images = Images{}
 	filepath.Walk(common.GetImagePath(), func(path string, info os.FileInfo, err error) error {
 		suffix := filepath.Ext(path)
 		//跳过文件夹
-		if info.IsDir() || (suffix != ".jpg" && suffix != ".jpeg" && suffix != ".png" && suffix != ".gif"){
+		if info.IsDir() || (suffix != ".jpg" && suffix != ".jpeg" && suffix != ".png" && suffix != ".gif") {
 			return nil
 		}
 
 		image := Image{}
 		//只获取web文件夹之后的路径
-		root := filepath.Join(config.GetRootPath(),"web")
+		root := filepath.Join(config.GetRootPath(), "web")
 		image.Url = path[len(root):]
 		image.CreateTime = info.ModTime()
 
-		images = append(images,image)
+		images = append(images, image)
 
 		return nil
 	})
@@ -56,10 +55,10 @@ func (list *ImageHost)ImageList() []Image {
 	return images
 }
 
-func (image *ImageHost)ImageUpload(ctx iris.Context) (string,error) {
+func (image *ImageHost) ImageUpload(ctx iris.Context) (string, error) {
 	file, info, err := ctx.FormFile("file")
 	if err != nil {
-		return "",errors.New("上传图片失败！")
+		return "", errors.New("上传图片失败！")
 	}
 	defer file.Close()
 
@@ -68,24 +67,24 @@ func (image *ImageHost)ImageUpload(ctx iris.Context) (string,error) {
 
 	//创建文件夹
 	date := time.Now().Format("2006-01-02")
-	path := filepath.Join(common.GetImagePath(),date)
-	err = os.MkdirAll(path,os.ModePerm)
+	path := filepath.Join(common.GetImagePath(), date)
+	err = os.MkdirAll(path, os.ModePerm)
 	if err != nil {
 		panic("文件夹创建失败！")
 	}
 
-	out,err := os.OpenFile(path + "/" +fname,os.O_WRONLY|os.O_CREATE, 0666)
+	out, err := os.OpenFile(path+"/"+fname, os.O_WRONLY|os.O_CREATE, 0666)
 	if err != nil {
-		return "",errors.New("保存图片失败！")
+		return "", errors.New("保存图片失败！")
 	}
 	defer out.Close()
-	_,err = io.Copy(out, file)
+	_, err = io.Copy(out, file)
 
 	if err != nil {
-		return "",errors.New("保存图片失败！")
+		return "", errors.New("保存图片失败！")
 	}
 
-	return "/" + config.String("image_host.img_dir") + "/" + fname,nil
+	return filepath.Join("/", config.String("image_host.img_dir"), date) + fname, nil
 }
 
 //实现了sort接口
